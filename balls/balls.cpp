eee0056f66169d640dc35fb16605941706245900
René Filip <renefilip@mail.com>
2020-03-20 14:02:10 +0100

    draw balls

--- balls/balls.cpp
+++ balls/balls.cpp
@@ -2,21 +2,77 @@
 #include "olcPixelGameEngine.h"
 
 #include <iostream>
-#include <vector>
 #include <string>
 
-using namespace std;
+struct sBall
+{
+    float px, py;
+    float vx, vy;
+    float ax, ay;
+    float radius;
+
+    int id;
+};
+
+class CirclePhysics : public olc::PixelGameEngine
+{
+public:
+    CirclePhysics()
+    {
+        sAppName = "Balls";
+    }
+
+private:
+    // TODO: use real balls later
+    // std::vector<std::pair<float, float>> modelCircle;
+    std::vector<sBall> vecBalls;
+    // sBall *pSelectedBall = nullptr;
+
+    void AddBall(float x, float y, float r = 5.0f)
+    {
+        sBall b;
+        b.px = x; b.py = y;
+        b.vx = 0; b.vy = 0;
+        b.ax = 0; b.ay = 0;
+        b.radius = r;
+
+        b.id = vecBalls.size();
+        vecBalls.emplace_back(b);
+    }
+
+public:
+    bool OnUserCreate() override
+    {
+        // modelCircle.push_back({ 0.0f, 0.0f });
+        // int nPoints = 20;
+        // for (int i = 0; i < nPoints; i++)
+        //     modelCircle.push_back({ cosf(i / (float)(nPoints - 1) * 2.0f * 3.14159f), sinf(i / (float)(nPoints - 1) * 2.0f * 3.14159f) });
+
+        float fDefaultRad = 8.0f;
+
+        for (int i = 0; i < 10; i++)
+            AddBall(rand() % ScreenWidth(), rand() % ScreenHeight(), rand() % 16 + 2);
+
+        return true;
+    }
+
+    bool OnUserUpdate(float fElapsedTime) override 
+    {
+        Clear(olc::BLACK);
+
+        // Draw Balls
+        for (auto ball : vecBalls)
+            DrawCircle(ball.px, ball.py, ball.radius, olc::WHITE);
+
+        return true;
+    }
+};
 
 int main()
 {
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
-    int a = 3;
-    msg.push_back("test");
-    for (const string& word : msg)
-    {
-        cout << word << " ";
-    }
-    cout << endl;
-    a = 4;
-    cout << "test" << endl;
+    CirclePhysics balls;
+    if (balls.Construct(160, 120, 8, 8))
+		balls.Start();
+
+	return 0;
 }
